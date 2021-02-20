6108549a9b9096b6c641eeac8b1d9699625a84f8
Briano Goestiawan <b@briano.io>
2021-02-20 11:43:14 +1100

    Add constant space solution

--- 2021-02-19:sum_of_two.py
+++ 2021-02-19:sum_of_two.py
@@ -27,7 +27,46 @@ def sum_of_two_v2(a, k):
     return False
 
 
-solution = sum_of_two_v2
+def sum_of_two_v3(a, k):
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    target = set()
+    for e in a:
+        if e in target:
+            return True
+        target.add(k-e)
+    return False
+
+
+def sum_of_two_v4(a, k):
+    """
+    Time:  O(n * log(n))
+    Space: O(1)
+    """
+    a.sort()
+    for i in range(len(a)):
+        target = k-a[i]
+        if binary_search(a, target) != -1:
+            return True
+    return False
+
+
+def binary_search(lst, e):
+    if len(lst) == 0:
+        return -1
+    if len(lst) == 1:
+        return 0 if lst[0] == e else -1
+    mid = len(lst) // 2
+    if lst[mid] == e:
+        return mid
+    left_i = binary_search(lst[:mid], e)
+    right_i = binary_search(lst[mid:], e)
+    return (left_i != -1 and left_i) or (right_i != -1 and right_i+mid) or -1
+
+
+solution = sum_of_two_v4
 
 a = [10, 15, 3, 7]
 k = 17
