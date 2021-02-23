24ae0c5fb94c409ccbce4aa533dc5e84be5c6377
Briano Goestiawan <b@briano.io>
2021-02-23 11:13:18 +1100

    solution

--- 2021-02-22:missing_positive_integer.py
+++ 2021-02-22:missing_positive_integer.py
@@ -1,5 +1,4 @@
 
-
 def missing_positive_integer(lst):
     """
     Time:  O(n^2)
@@ -45,3 +44,18 @@ for inp, out in test_cases:
     print("expected:", out)
     print("got:     ", solution(inp))
     print()
+
+
+
+def first_missing_positive(nums):
+    if not nums:
+        return 1
+    for i, _ in enumerate(nums):
+        while i + 1 != nums[i] and 0 < nums[i] <= len(nums):
+            nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
+            if nums[i] == nums[nums[i] - 1]:
+                break
+    for i, num in enumerate(nums, 1):
+        if num != i:
+            return i
+    return len(nums) + 1
