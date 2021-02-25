86a5f937ea02b5a43015ee0b270ff374420740c3
Briano Goestiawan <b@briano.io>
2021-02-25 13:30:36 +1100

    learned ctypes

--- 2021-02-24:xor_linked_list.py
+++ 2021-02-24:xor_linked_list.py
@@ -33,11 +33,58 @@ class XORLinkedList:
         return curr.value
 
 
-l = XORLinkedList()
-l.add(3)
-l.add(1)
-l.add(12)
-solution = l.get
+import ctypes
+
+
+# This is hacky. It's a data structure for C, not python.
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+        self.both = 0
+
+
+class XorLinkedList(object):
+    def __init__(self):
+        self.head = self.tail = None
+        self.__nodes = [] # This is to prevent garbage collection
+
+    def add(self, node):
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            self.tail.both = id(node) ^ self.tail.both
+            node.both = id(self.tail)
+            self.tail = node
+
+        # Without this line, Python thinks there is no way to reach nodes between
+        # head and tail.
+        self.__nodes.append(node)
+
+
+    def get(self, index):
+        prev_id = 0
+        node = self.head
+        for i in range(index):
+            next_id = prev_id ^ node.both
+
+            if next_id:
+                prev_id = id(node)
+                node = _get_obj(next_id)
+            else:
+                raise IndexError('Linked list index out of range')
+        return node
+
+
+def _get_obj(id):
+    return ctypes.cast(id, ctypes.py_object).value
+
+
+l = XorLinkedList()
+l.add(Node(3))
+l.add(Node(1))
+l.add(Node(12))
+# solution = lambda x: ctypes.cast(id(l.get(x)), ctypes.py_object).value.val
+solution = lambda x: l.get(x).val
 
 test_cases = [
     # input, output
